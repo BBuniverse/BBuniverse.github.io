454b9d921f730d58fa0dffccdd3329c78c4687ed
bowen <bbuniverse@outlook.com>
2021-05-19 15:46:03 +0800

    Validation checked

--- js/feedsFilter.js
+++ js/feedsFilter.js
@@ -3,10 +3,12 @@ function filter(obj) {
         var bound = document.getElementById("input")
         var boxs = document.querySelectorAll("div.feeds");
         for (i = 0; i < boxs.length; i++) {
+            // Show all item at first
             boxs[i].style.display = "";
             if (i < bound.value) {
                 boxs[i].style.background = "#fffadd";
             } else {
+                // Hidden reset item
                 boxs[i].style.display = "none";
             }
         }
